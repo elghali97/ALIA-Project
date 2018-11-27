d31bfe35674e7a47cb042fb512767a35efd5fd9f
Arthur BELLEMIN <arthurbellemin11@gmail.com>
2018-11-27 10:34:46 +0100

    Piece et colonne v2

--- puissance4.pl
+++ puissance4.pl
@@ -14,7 +14,9 @@ column(I,N):-I>0, I<8, N>0, N<7.
 % 			false : column(0,5),column(2,15)
 
 % Prédicat piece(num col, num line, color)
-piece(X,Y,C):-X>0, X<8, Y>0, Y<7, member(C,['y','r']),!.
-% Tests : 	true : piece(1,1,'y'),piece(7,6,'r')
+piece(X,Y,'r'):-X>0, X<8, Y>0, Y<7. 	% red
+piece(X,Y,'y'):-X>0, X<8, Y>0, Y<7. 	% yellow
+piece(X,Y,'?'):-X>0, X<8, Y>0, Y<7. 	% empty case
+% Tests : 		true : piece(1,1,'y'),piece(7,6,'r')
 % 			false : piece(11,6,'r'),piece(5,6,'z')
 % 			valeur : piece(1,1,C)
