13ae22451ed361c24070ea5a33594b1264602156
ghalib2015 <44357847+ghalib2015@users.noreply.github.com>
2018-11-27 11:28:08 +0100

    Update puissance4.pl

--- puissance4.pl
+++ puissance4.pl
@@ -3,12 +3,19 @@
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+:-dynamic column/2.
+:-dynamic piece/3.
+
+init:-init_c(7),init_p(7,6).
+
+init_c(0).
+init_c(N):-assert(column(N,0)),N1 is N-1,init_c(N1).
 
 
 play(Player) :- write('New turn for:'), writeln(Player),
 		displayBoard %in order to print it
 		
-% Prédicat piece(num col, num line, color)
+% PrÃ©dicat piece(num col, num line, color)
 piece(1,6,'r').
 piece(2,6,'r').
 piece(3,6,'r').
@@ -23,20 +30,29 @@ piece(X,Y,'y'):-X>0, X<8, Y>0, Y<7. 	% yellow
 % 			false : piece(11,6,'r'),piece(5,6,'z')
 % 			valeur : piece(1,1,C)
 
-% Prédicat displayBoard (1,6) pour afficher
+% PrÃ©dicat displayBoard (1,6) pour afficher
 displayBoard(7,1):-piece(7,1,C), write(C), writeln(' | '), writeln('__________________').
 displayBoard(1,Y):-piece(1,Y,C), writeln('__________________'), write(' | '),
     write(C), write(' | '), displayBoard(2,Y).
 displayBoard(7,Y):-piece(7,Y,C), write(C), writeln(' | '), Y1 is Y-1, displayBoard(1,Y1).
 displayBoard(X,Y):-piece(X,Y,C), write(C), write(' | '), X1 is X+1, displayBoard(X1,Y).
 
-% Prédicat isBoardFull (1) pour vérifier si la grille est pleine
+% PrÃ©dicat isBoardFull (1) pour vÃ©rifier si la grille est pleine
 isBoardFull(7):-piece(7,6,C),!, C\=='?'.
 isBoardFull(X):-piece(X,6,C),!, C\=='?', X1 is X+1, isBoardFull(X1).
 % Tests : 	true : piece(1,6,'r'),piece(2,6,'r'),piece(3,6,'r'),piece(4,6,'r'),piece(5,6,'r'),piece(6,6,'r'),piece(7,6,'r').
 % 			false : piece(1,6,'?'),piece(2,6,'?'),piece(3,6,'?'),piece(4,6,'?'),piece(5,6,'?'),piece(6,6,'r'),piece(7,6,'?')
 % 					piece(1,6,'?'),piece(2,6,'r'),piece(3,6,'?'),piece(4,6,'?'),piece(5,6,'r'),piece(6,6,'r'),piece(7,6,'?')
 
-% Prédicat endGame(Winner si on est en conf gagnante ou 'Draw' si la grill est pleine)
+% PrÃ©dicat endGame(Winner si on est en conf gagnante ou 'Draw' si la grill est pleine)
 endGame(Winner):-winner(Winner).
 endGame('Draw'):-isBoardFull.
+
+add(NC,Player):-
+    column(NC,N),
+    N<7,
+    N1 is N+1,
+    retract(piece(NC,N1,'?')),
+    asserta(piece(NC,N1,Player)),
+    retract(column(NC,N)),
+    assert(column(NC,N1)).
