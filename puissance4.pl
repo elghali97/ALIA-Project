06223f4858664bc0f6d50433ac2f1c734d604c70
ghalib2015 <44357847+ghalib2015@users.noreply.github.com>
2018-11-28 20:58:40 +0100

    Update puissance4.pl
    Correction du code existant

--- puissance4.pl
+++ puissance4.pl
@@ -1,52 +1,31 @@
-%%%%%%TODO HERE PUT USEFUL GENERAL PREDICATES %%%%%%%%
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 :-dynamic column/2.
 :-dynamic piece/3.
 /*init du jeu*/
-init:-init_c(7),init_p(7,6). 
-/*init des colonnes a 0, init des pieces a ?*/
-init_c(0).
-init_c(N):-assert(column(N,0)),N1 is N-1,init_c(N1).
+init:-init_c(7),init_p(7,6),play('r'). 
+/*init des colonnes à 0*/
+init_c(0):-!.
+init_c(N):-assert(column(N,0)),N1 is N-1,init_c(N1),!.
 
+/* init des pieces à ?*/
+init_p(0,6):-!.
+init_p(I,1):-assert(piece(I,1,'?')),I1 is I-1,init_p(I1,6),!.
+init_p(I,J):-assert(piece(I,J,'?')),J1 is J-1,init_p(I,J1),!.
 
 play(Player) :- write('New turn for:'), writeln(Player),
-		displayBoard %in order to print it
-		
-% Prédicat piece(num col, num line, color)
-piece(1,6,'r').
-piece(2,6,'r').
-piece(3,6,'r').
-piece(4,6,'r').
-piece(5,6,'?').
-piece(6,6,'r').
-piece(7,6,'r').
-piece(X,Y,'?'):-X>0, X<8, Y>0, Y<7. 	% empty case
-piece(X,Y,'r'):-X>0, X<8, Y>0, Y<7. 	% red
-piece(X,Y,'y'):-X>0, X<8, Y>0, Y<7. 	% yellow
-% Tests : 	true : piece(1,1,'y'),piece(7,6,'r')
-% 			false : piece(11,6,'r'),piece(5,6,'z')
-% 			valeur : piece(1,1,C)
+		displayBoard(1,6), /*in order to print it*/
+        move(Player),
+    	not(endGame(Player)),
+    	changePlayer(Player,Player1),
+        play(Player1).
 
-% Prédicat displayBoard (1,6) pour afficher
-displayBoard(7,1):-piece(7,1,C), write(C), writeln(' | '), writeln('__________________').
-displayBoard(1,Y):-piece(1,Y,C), writeln('__________________'), write(' | '),
-    write(C), write(' | '), displayBoard(2,Y).
-displayBoard(7,Y):-piece(7,Y,C), write(C), writeln(' | '), Y1 is Y-1, displayBoard(1,Y1).
-displayBoard(X,Y):-piece(X,Y,C), write(C), write(' | '), X1 is X+1, displayBoard(X1,Y).
+changePlayer('r','y').
+changePlayer('y','r').
 
-% Prédicat isBoardFull (1) pour vérifier si la grille est pleine
-isBoardFull(7):-piece(7,6,C),!, C\=='?'.
-isBoardFull(X):-piece(X,6,C),!, C\=='?', X1 is X+1, isBoardFull(X1).
-% Tests : 	true : piece(1,6,'r'),piece(2,6,'r'),piece(3,6,'r'),piece(4,6,'r'),piece(5,6,'r'),piece(6,6,'r'),piece(7,6,'r').
-% 		false : piece(1,6,'?'),piece(2,6,'?'),piece(3,6,'?'),piece(4,6,'?'),piece(5,6,'?'),piece(6,6,'r'),piece(7,6,'?')
-% 			piece(1,6,'?'),piece(2,6,'r'),piece(3,6,'?'),piece(4,6,'?'),piece(5,6,'r'),piece(6,6,'r'),piece(7,6,'?')
+/* Prédicat displayBoard (1,6) pour afficher */
+displayBoard(7,1):-piece(7,1,X),write(' | '),write(X),writeln(' | '),writeln('_______________________').
+displayBoard(7,J):-piece(7,J,X),write(' | '),write(X),writeln(' | '),writeln('_______________________'),J1 is J-1,displayBoard(1,J1),!.
+displayBoard(I,J):-piece(I,J,X),write(' | '),write(X),write(' | '),I1 is I+1,displayBoard(I1,J),!.
 
-% Prédicat endGame(Winner si on est en conf gagnante ou 'Draw' si la grille est pleine)
-endGame(Winner):-checkStatus(X,Y,Winner).
-endGame('Draw'):-isBoardFull.
 
 /*ajout d'un jeton dans une position valide*/
 add(NC,Player):-
@@ -59,6 +38,16 @@ add(NC,Player):-
 assert(column(NC,N1)).
 
 
+/* Prédicat isBoardFull (1) pour vérifier si la grille est pleine */
+isBoardFull(7):-column(7,C), C==7,writeln('Egalite').
+isBoardFull(X):-column(X,C), C==7, X1 is X+1, isBoardFull(X1).
+
+/*endGame(Player):-checkStatus(Player),!,abort.*/
+endGame(_):-isBoardFull(1),!,abort.
+
+/* predicat permettant a un utilisateur de jouer*/
+move(Player):-repeat,read(X),X>0,X<8,column(X,N),N<6,add(X,Player).
+
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    CONDITIONS DE FIN DU JEU POUR SAVOIR SI UN JOUEUR A GAGNE/PERDU
    
@@ -69,18 +58,21 @@ assert(column(NC,N1)).
    - Alignement de quatre pièces de même couleur dans la direction de la diagonale secondaire
    
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-%Verification d'un alignement gagnant à partir d'une pièce (X,Y) avec sa couleur
+/*Verification d'un alignement gagnant à partir d'une pièce (X,Y) avec sa couleur*/
+/*
+checkStatus(Player):-piece(X,Y,Player),!,checkStatus(X,Y,Player).
 checkStatus(_,_,_).
 checkStatus(X,Y,Color):-
 	fourInARowCheck(X,Y),
 	(   Color == r -> write('Le joueur rouge a gagné !');
 	    Color == y -> write('Le joueur jaune a gagné !')
 	).
-
-% Comptage du nombre de pièce de même couleur à la suite dans une direction donnée
-% - (X,Y) la position de la pièce dans le jeu
-% - (DirectX, DirectY) le vecteur directeur de la direction dans laquelle on vérifie l'alignement
-% - sumInARow le nombre de pièces de même couleur à la suite trouvées dans cette direction
+*/
+/* Comptage du nombre de pièce de même couleur à la suite dans une direction donnée
+ - (X,Y) la position de la pièce dans le jeu
+ - (DirectX, DirectY) le vecteur directeur de la direction dans laquelle on vérifie l'alignement
+ - sumInARow le nombre de pièces de même couleur à la suite trouvées dans cette direction
+*/
 numberInARow(_,_,_,_,1).
 numberInARow(X,Y,DirectX,DirectY,SumInARow):-
     NewX is X+DirectX,
@@ -91,28 +83,28 @@ numberInARow(X,Y,DirectX,DirectY,SumInARow):-
     numberInARow(NewX,NewY,DirectX,DirectY,NewSum),
     SumInARow is NewSum +1,!.
 
-%Verification d'un alignement horizontal
+/*Verification d'un alignement horizontal*/
 fourInARowCheck(X,Y):-
     numberInARow(X,Y,1,0,Sum1),
     numberInARow(X,Y,-1,0,Sum2),
     Sum is Sum1+Sum2-1,
     Sum>=4,!.
 
-%Verification d'un alignement vertical
+/*Verification d'un alignement vertical*/
 fourInARowCheck(X,Y):-
     numberInARow(X,Y,0,1,Sum1),
     numberInARow(X,Y,0,-1,Sum2),
     Sum is Sum1+Sum2-1,
     Sum>=4,!.
 
-%Verification d'un alignement dans la direction de la diagonale principale
+/*Verification d'un alignement dans la direction de la diagonale principale*/
 fourInARowCheck(X,Y):-
     numberInARow(X,Y,-1,1,Sum1),
     numberInARow(X,Y,1,-1,Sum2),
     Sum is Sum1+Sum2-1,
     Sum>=4,!.
 
-%Verification d'un alignement dans la direction de la diagonale secondaire
+/*Verification d'un alignement dans la direction de la diagonale secondaire*/
 fourInARowCheck(X,Y):-
     numberInARow(X,Y,1,1,Sum1),
     numberInARow(X,Y,-1,-1,Sum2),
